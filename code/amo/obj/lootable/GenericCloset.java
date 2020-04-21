1bef68555613240f05560009bf249f39f6a72878
Lizurt <lizard.x@mail.ru>
2020-04-21 13:17:49 +0300

    sprite for locker

--- code/amo/obj/lootable/GenericCloset.java
+++ code/amo/obj/lootable/GenericCloset.java
@@ -3,6 +3,7 @@ package amo.obj.lootable;
 import amo.area.Area;
 import amo.obj.Obj;
 import amo.Size;
+import javafx.scene.image.Image;
 import util.Random;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class GenericCloset extends Obj implements Lootable {
     public GenericCloset(Area holder) {
         super(Random.pick("", "непримечательный ", "серый ", "стальной ", "старый ") +  "шкафчик", holder);
         setSize(Size.HUGE);
+        setIcon(new Image(getClass().getResourceAsStream("/icons/objects/items/lootable/locker.png")));
     }
 
     @Override
